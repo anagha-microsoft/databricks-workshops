843fad374689d597df3baf273497991878673174
Anagha Khanolkar <ankhanol@microsoft.com>
2018-09-08 21:32:07 -0500

    Update: README.md data-engineering data-science nosql-cosmosdb-hol nosql notebooks

---
+++ nosql/cosmosdb/00-Setup/06-Libraries/ReadMe.scala
@@ -0,0 +1,13 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC # Libraries
+// MAGIC 
+// MAGIC ####Cassandra API Hands-On-Lab:<br>
+// MAGIC For the Cassandra hands on lab:<br>
+// MAGIC 1) You will need the datastax cassandra connector from maven central that is compatible with the Dataricks runtime<br>
+// MAGIC 2) You will need the CosmosDB Cassandra API related jar from maven central<br>
+// MAGIC 3) You can save the libraries ot this directory or to a common/shared location<br>
+// MAGIC <br>
+// MAGIC Follow instructions detailed here to attach libaries to your cluster-<br>
+// MAGIC https://docs.databricks.com/user-guide/libraries.html<br>
+// MAGIC Refer to the section - "Upload a Maven package or Spark package" and follow the steps to attach the connector library to the cluster.<br>
