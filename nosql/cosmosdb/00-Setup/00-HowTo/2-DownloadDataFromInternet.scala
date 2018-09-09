860f5d328f15fe75c2f68918cc662470888b4b4b
Anagha Khanolkar <ankhanol@microsoft.com>
2018-09-08 21:02:30 -0500

    Update: README.md data-engineering data-science nosql-cosmosdb-hol nosql notebooks

---
+++ nosql/cosmosdb/00-Setup/00-HowTo/2-DownloadDataFromInternet.scala
@@ -0,0 +1,8 @@
+// Databricks notebook source
+// MAGIC 
+// MAGIC %md
+// MAGIC # Download data from the internet
+
+// COMMAND ----------
+
+//This is covered in section 5 of the setup module - 00-Setup/05-Dataset/2-DownloadDataset
