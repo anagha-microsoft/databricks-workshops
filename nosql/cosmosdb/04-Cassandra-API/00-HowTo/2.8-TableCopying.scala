51edf970ef853116edda8954f3eda904db062cb3
Anagha Khanolkar <ankhanol@microsoft.com>
2018-09-10 00:20:59 -0500

    Update: README.md data-engineering data-science nosql-cosmosdb-hol nosql notebooks

---
+++ nosql/cosmosdb/04-Cassandra-API/00-HowTo/2.8-TableCopying.scala
@@ -0,0 +1,111 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC # What's in this exercise
+// MAGIC Basics of how to copy data from one table to another<B>in batch</B>.<BR>
+
+// COMMAND ----------
+
+// MAGIC %run ../00-HowTo/2-CassandraUtils
+
+// COMMAND ----------
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+import spark.implicits._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.Column
+import org.apache.spark.sql.types.{StructType, StructField, StringType, IntegerType,LongType,FloatType,DoubleType, TimestampType}
+import org.apache.spark.sql.cassandra._
+
+//datastax Spark connector
+import com.datastax.spark.connector._
+import com.datastax.spark.connector.cql.CassandraConnector
+
+//CosmosDB library for multiple retry
+import com.microsoft.azure.cosmosdb.cassandra
+
+// Specify connection factory for Cassandra
+spark.conf.set("spark.cassandra.connection.factory", "com.microsoft.azure.cosmosdb.cassandra.CosmosDbConnectionFactory")
+
+// Parallelism and throughput configs
+spark.conf.set("spark.cassandra.output.batch.size.rows", "1")
+spark.conf.set("spark.cassandra.connection.connections_per_executor_max", "10")
+spark.conf.set("spark.cassandra.output.concurrent.writes", "100")
+spark.conf.set("spark.cassandra.concurrent.reads", "512")
+spark.conf.set("spark.cassandra.output.batch.grouping.buffer.size", "1000")
+spark.conf.set("spark.cassandra.connection.keep_alive_ms", "60000000") //Increase this number as needed
+spark.conf.set("spark.cassandra.output.consistency.level","ALL")//Write consistency = Strong
+spark.conf.set("spark.cassandra.input.consistency.level","ALL")//Read consistency = Strong
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## 8.0.1. Read from one table, save to another existing table
+
+// COMMAND ----------
+
+//1) Create destination table
+val cdbConnector = CassandraConnector(sc)
+cdbConnector.withSessionDo(session => session.execute("CREATE TABLE IF NOT EXISTS books_ks.books_copy(book_id TEXT PRIMARY KEY,book_author TEXT, book_name TEXT,book_pub_year INT,book_price FLOAT) WITH cosmosdb_provisioned_throughput=4000;"))
+
+//2) Read from one table
+val readBooksDF = sqlContext
+  .read
+  .format("org.apache.spark.sql.cassandra")
+  .options(Map( "table" -> "books", "keyspace" -> "books_ks"))
+  .load
+
+//3) Save to destination table
+readBooksDF.write
+  .cassandraFormat("books_copy", "books_ks", "")
+  .save()
+
+// COMMAND ----------
+
+sqlContext
+  .read
+  .format("org.apache.spark.sql.cassandra")
+  .options(Map( "table" -> "books_copy", "keyspace" -> "books_ks"))
+  .load
+  .show
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## 8.0.2. CTAS
+
+// COMMAND ----------
+
+import com.datastax.spark.connector._
+
+//1) Read from source table
+val readBooksDF = sqlContext
+  .read
+  .format("org.apache.spark.sql.cassandra")
+  .options(Map( "table" -> "books", "keyspace" -> "books_ks"))
+  .load
+
+//2) Creates an empty table in the keyspace based off of source table
+val newBooksDF = readBooksDF
+newBooksDF.createCassandraTable(
+    "books_ks", 
+    "books_new", 
+    partitionKeyColumns = Some(Seq("book_id"))
+    //clusteringKeyColumns = Some(Seq("some column"))
+    )
+
+//3) Saves the data from the source table into the newly created table
+newBooksDF.write
+  .cassandraFormat("books_new", "books_ks","")
+  .save()
+
+
+// COMMAND ----------
+
+sqlContext
+  .read
+  .format("org.apache.spark.sql.cassandra")
+  .options(Map( "table" -> "books_new", "keyspace" -> "books_ks"))
+  .load
+  .show
