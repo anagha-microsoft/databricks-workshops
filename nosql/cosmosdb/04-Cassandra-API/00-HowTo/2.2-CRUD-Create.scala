38178348a6748e76a7fdbf9eb6369770abe8f030
Anagha Khanolkar <ankhanol@microsoft.com>
2018-09-14 14:09:27 -0700

    Update: README.md data-engineering data-science nosql

--- nosql/cosmosdb/04-Cassandra-API/00-HowTo/2.2-CRUD-Create.scala
+++ nosql/cosmosdb/04-Cassandra-API/00-HowTo/2.2-CRUD-Create.scala
@@ -15,10 +15,6 @@
 
 // COMMAND ----------
 
-// MAGIC %run ../00-HowTo/2-CassandraUtils
-
-// COMMAND ----------
-
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
