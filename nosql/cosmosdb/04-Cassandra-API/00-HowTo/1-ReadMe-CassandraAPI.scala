61df38a1579a3a0eb69ffd38d2b6de412cff3d80
Anagha Khanolkar <ankhanol@microsoft.com>
2018-09-15 22:45:13 -0700

    Update: README.md data-engineering data-science nosql

--- nosql/cosmosdb/04-Cassandra-API/00-HowTo/1-ReadMe-CassandraAPI.scala
+++ nosql/cosmosdb/04-Cassandra-API/00-HowTo/1-ReadMe-CassandraAPI.scala
@@ -1,17 +1,15 @@
 // Databricks notebook source
 // MAGIC %md
-// MAGIC # CosmosDB Cassandra API
+// MAGIC # Azure Cosmos DB Cassandra API
 // MAGIC 
-// MAGIC CosmosDB Cassandra API allows you to interact with CosmosDB using Cassandra constructs/codebase. CosmosDB is a managed service, therefore, not all functionality available in Cassandra is applicable for the Cassandra API for CosmosDB.  E.g. CosmosDB has its own replication,indexing etc that is not overridable.<br><br>
-// MAGIC 
-// MAGIC Documentation: **TODO** - add link here<br>
+// MAGIC The Cassandra API allows you to interact with Azure Cosmos DB using Cassandra constructs/codebase. Azure Cosmos DB is a managed service, therefore, not all functionality available in Cassandra is applicable.  E.g. Azure Cosmos DB has its own replication,indexing etc that is not overridable.<br>
 // MAGIC 
 // MAGIC This module covers:<br>
-// MAGIC 1) Provisioning CosmosDb - Cassandra API<br>
-// MAGIC 2) CosmosDB configuration needed for Spark integration<br>
+// MAGIC 1) Provisioning Azure Cosmos DB - Cassandra API<br>
+// MAGIC 2) Azure Cosmos DB configuration needed for Spark integration<br>
 // MAGIC 3) Attaching Spark-Cassandra connector library to the cluster<br>
-// MAGIC 4) Attaching CosmosDB Spark-Cassandra specific library<br>
-// MAGIC 5) Cluster-wide configuration of CosmosDB instance details<br>
+// MAGIC 4) Attaching Azure Cosmos DB Spark-Cassandra specific library<br>
+// MAGIC 5) Cluster-wide configuration of Azure Cosmos DB instance details<br>
 // MAGIC 6) Important links<br>
 // MAGIC 7) Installing cqlsh for DDL operations<br>
 // MAGIC 8) Need support?
@@ -19,14 +17,14 @@
 // COMMAND ----------
 
 // MAGIC %md
-// MAGIC ### 1.0. Provision CosmosDB Cassandra API instance
+// MAGIC ### 1.0. Provision Azure Cosmos DB Cassandra API instance
 // MAGIC Details can be found here-<br>
 // MAGIC https://docs.microsoft.com/en-us/azure/cosmos-db/create-cassandra-dotnet#create-a-database-account
 
 // COMMAND ----------
 
 // MAGIC %md
-// MAGIC ### 2.0. CosmosDB instance related configuration needed for Spark integration
+// MAGIC ### 2.0. Azure Cosmos DB instance related configuration needed for Spark integration
 // MAGIC The following lists configuration key-value pairs:<br>
 // MAGIC 1) spark.cassandra.connection.host: YOUR_COSMOSDB_ACCOUNT_NAME.cassandra.cosmosdb.azure.com<br>
 // MAGIC 2) spark.cassandra.connection.port: 10350<br>
@@ -38,7 +36,7 @@
 
 // MAGIC %md
 // MAGIC ### 3.0. Attaching Spark-Cassandra connector library to the cluster
-// MAGIC To work with CosmosDB Cassandra API, we have to attach the Datastax Cassandra connector the cluster.<br>
+// MAGIC To work with Azure Cosmos DB Cassandra API, we have to attach the Datastax Cassandra connector the cluster.<br>
 // MAGIC 1.  Review the Databricks runtime version - to see the associated Spark version and find associated maven coordinates for the compatible Datastax Cassandra Spark connector-<br>
 // MAGIC https://mvnrepository.com/artifact/com.datastax.spark/spark-cassandra-connector
 // MAGIC 2.  Attach the connector to the cluster<br>
@@ -49,7 +47,7 @@
 // COMMAND ----------
 
 // MAGIC %md
-// MAGIC ### 4.0. Attaching CosmosDB-Cassandra API specific library to the cluster
+// MAGIC ### 4.0. Attaching Azure Cosmos DB-Cassandra API specific library to the cluster
 // MAGIC We need a custom connection factory as this is the only way to configure a retry policy on the connector.<br>
 // MAGIC As completed in 3.0., add the following maven coordinates to attach the jar to the cluster<br>
 // MAGIC Download: https://search.maven.org/artifact/com.microsoft.azure.cosmosdb/azure-cosmos-cassandra-spark-helper/1.0.0/jar<br>
@@ -58,9 +56,9 @@
 // COMMAND ----------
 
 // MAGIC %md
-// MAGIC ### 5.0. Cluster-wide configuration of CosmosDB instance details
+// MAGIC ### 5.0. Cluster-wide configuration of Azure Cosmos DB instance details
 // MAGIC With Spark 2.x, the entry point is Spark session.<br>
-// MAGIC With the Datastax Spark connector, th dataframe API can read configuration off of that defined at a spark session level within notebook.<br>
+// MAGIC With the Datastax Spark connector, the dataframe API can read configuration off of that defined at a spark session level within notebook.<br>
 // MAGIC NOTE: For the RDD API though, we need to use the Spark context which is initialized at cluster start-up.  We therefore need to add the details in section 2.0 at a cluster level.<br>
 // MAGIC 
 // MAGIC The following are options for ways to set up cluster wide conf:<br>
@@ -95,8 +93,8 @@
 // COMMAND ----------
 
 // MAGIC %md
-// MAGIC ### 7.0. Using cqlsh with CosmosDB Cassandra API
-// MAGIC If you are particular about using cqlsh, you can install a pseudo-distrubuted Cassandra instance on your machine, and launch cqlsh against CosmosDB-Cassandra API instance.<br><br>
+// MAGIC ### 7.0. Using cqlsh with Azure Cosmos DB Cassandra API
+// MAGIC If you are particular about using cqlsh, you can install a pseudo-distrubuted Cassandra instance on your machine, and launch cqlsh against Azure Cosmos DB-Cassandra API instance.<br><br>
 // MAGIC **1) Installing - cqlsh:**
 // MAGIC - Installation on Mac<br>
 // MAGIC <code>"sudo brew install cassandra"</code> and you are done!
@@ -111,7 +109,7 @@
 // MAGIC <code>bin/cqlsh</code>
 // MAGIC 
 // MAGIC <br>
-// MAGIC **2) Launching cqlsh against CosmosDB Cassandra API:**<bin>
+// MAGIC **2) Launching cqlsh against Azure Cosmos DB Cassandra API:**<bin>
 // MAGIC <code>
 // MAGIC cd bin<br>
 // MAGIC export SSL_VERSION=TLSv1_2<br>
