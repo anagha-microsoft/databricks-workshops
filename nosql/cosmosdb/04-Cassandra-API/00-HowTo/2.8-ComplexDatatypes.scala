f78defb65e174e294d67d6b6a2fec8b803d8c431
Anagha Khanolkar <ankhanol@microsoft.com>
2018-09-18 10:15:19 -0700

    Update: nosql

---
+++ nosql/cosmosdb/04-Cassandra-API/00-HowTo/2.8-ComplexDatatypes.scala
@@ -0,0 +1,34 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC # What's in this exercise
+// MAGIC Basics of how to work with Azure Cosmos DB - Cassandra API from Databricks in batch.<br>
+// MAGIC 8.0. Complex datatypes<BR>
+
+// COMMAND ----------
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+import spark.implicits._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.Column
+import org.apache.spark.sql.types.{StructType, StructField, StringType, IntegerType,LongType,FloatType,DoubleType, TimestampType}
+import org.apache.spark.sql.cassandra._
+
+//datastax Spark connector
+import com.datastax.spark.connector._
+import com.datastax.spark.connector.cql.CassandraConnector
+
+//CosmosDB library for multiple retry
+import com.microsoft.azure.cosmosdb.cassandra
+
+// Specify connection factory for Cassandra
+spark.conf.set("spark.cassandra.connection.factory", "com.microsoft.azure.cosmosdb.cassandra.CosmosDbConnectionFactory")
+
+// Parallelism and throughput configs
+spark.conf.set("spark.cassandra.output.batch.size.rows", "1")
+spark.conf.set("spark.cassandra.connection.connections_per_executor_max", "10")
+spark.conf.set("spark.cassandra.output.concurrent.writes", "100")
+spark.conf.set("spark.cassandra.concurrent.reads", "512")
+spark.conf.set("spark.cassandra.output.batch.grouping.buffer.size", "1000")
+spark.conf.set("spark.cassandra.connection.keep_alive_ms", "60000000") //Increase this number as needed
