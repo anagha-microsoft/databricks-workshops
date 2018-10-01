6d5c9e6525147fac0c86e4b6b9b469f3aa6f4df6
Anagha Khanolkar <ankhanol@microsoft.com>
2018-10-01 15:00:59 -0500

    Update: README.md data-engineering data-science nosql

--- nosql/cosmosdb/04-Cassandra-API/scala/00-HowTo/2.10-StreamIngest-FromKafka-Part3.scala
+++ nosql/cosmosdb/04-Cassandra-API/scala/00-HowTo/2.10-StreamIngest-FromKafka-Part3.scala
@@ -1,17 +1,25 @@
 // Databricks notebook source
 // MAGIC %md
 // MAGIC # What's in this exercise
-// MAGIC This is part 3 of 3 notebooks that demonstrate stream ingest from Kafka, of 1.5 GB of the Chicago crimes public dataset.<BR>
+// MAGIC This is part 2 of 4 notebooks that demonstrate stream ingest from Kafka, of 1.5 GB of the Chicago crimes public dataset.<BR>
 // MAGIC - In notebook 1, we published data to Kafka for purpose of the exercise<BR>
-// MAGIC - In notebook 2, we ingested from Kafka using structured stream processing and persisted to a Azure Cosmos DB Cassandra table<BR>
-// MAGIC - In this notebook, we will ingest from Kafka using structured stream processing and persist to a Databricks Delta table<BR>
+// MAGIC - In this notebook, we will (attemot to) ingest from Kafka using structured stream processing and persist to an Azure Cosmos DB Cassandra table<BR>
+// MAGIC - In notebook 3, we will ingest from Kafka using classic stream processing (DStream based) and persist to an Azure Cosmos DB Cassandra table<BR>
+// MAGIC - In notebook 4, we will ingest from Kafka using structured stream processing and persist to a Databricks Delta table<BR>
 // MAGIC   
 // MAGIC While the Azure Cosmos DB Cassandra table serves as a hot store for OLTP, the Delta table will serve as an analytics store.
+// MAGIC   
+// MAGIC ### Note:
+// MAGIC At the time of authoring this notebook (Oct 2018), **neither documented path to structured stream sink to Cassandra worked** (Spark 2.3.1, connector 2.11-2.3.2).<br>
+// MAGIC https://docs.datastax.com/en/dse/6.0/dse-dev/datastax_enterprise/spark/structuredStreaming.html<br>
+// MAGIC The published samples work only with connector and spark version 2.3.0, which does not work with Databricks runtime with Spark 2.3.0.<br>
+// MAGIC The connector author is working on forward compatibility tracked under [JIRA](https://www.google.com/url?q=https%3A%2F%2Fdatastax.jira.com%2Fbrowse%2FDSP-16635&sa=D&sntz=1&usg=AFQjCNED-nPzpF0jwURzMq7ibtNFQKfAXA)<br>
+// MAGIC Reference: [Structured streaming sink related thread](https://groups.google.com/a/lists.datastax.com/forum/#!msg/spark-connector-user/0aHJ4oskw7Q/xPKoqrtVAgAJ)<br>
 
 // COMMAND ----------
 
 // MAGIC %md
-// MAGIC ### 1.0. Read stream from Kafka
+// MAGIC ### 1.0. Read from Kafka with structured streaming
 
 // COMMAND ----------
 
@@ -76,54 +84,69 @@ consumableDF.printSchema
 
 // COMMAND ----------
 
-//consumableDF.show
-//org.apache.spark.sql.AnalysisException: Queries with streaming sources must be executed with writeStream.start();;
-
-// COMMAND ----------
-
 // MAGIC %md
-// MAGIC ### 3.0. Sink to Databricks Delta
+// MAGIC ### 3.0. Persist the stream to Azure Cosmos DB Cassandra API
 
 // COMMAND ----------
 
-// MAGIC %md
-// MAGIC ##### 3.0.1. Sink to delta table
-
-// COMMAND ----------
-
-//1) Destination directory for delta table, and for checkpoints
-val dbfsDestDirPath="/mnt/data/crimes/curatedDir/chicago-crimes-stream-delta/"
-val dbfsCheckpointDirPath="/mnt/data/crimes/checkpointDir/chicago-crimes-stream-delta/"
+//1) Destination directory for checkpoints
+val dbfsCheckpointDirPath="/mnt/data/crimes/checkpointDir/chicago-crimes-stream-cassandra/"
 
 //2) Remove output from prior execution
-dbutils.fs.rm(dbfsDestDirPath, recurse=true)
 dbutils.fs.rm(dbfsCheckpointDirPath, recurse=true)
 
-//3) Persist as delta format (Parquet) to curated zone to a delta table
-consumableDF.writeStream
-  .format("delta")
-  .outputMode("append")
+// COMMAND ----------
+
+//Author's attempt: 
+//Provisioning specs - Databricks: Runtime 4.3, 8 DS3v2 workers with option to scale to 10 workers, 
+//Provisioning specs - Cosmos DB: Started the table with 10K RUs;  Altered table to 10,000 RUs per physical partition created
+//Connector conf - spark.cassandra.output.batch.size.rows=1; spark.cassandra.connection.connections_per_executor_max=2; spark.cassandra.output.concurrent.writes=2;spark.cassandra.output.batch.grouping.buffer.size=300
+//The above needs tuning for sure
+
+
+//datastax Spark connector
+import com.datastax.spark.connector._
+import com.datastax.spark.connector.cql.CassandraConnector
+import org.apache.spark.sql.cassandra._
+
+//CosmosDB library for multiple retry
+import com.microsoft.azure.cosmosdb.cassandra
+
+//Specify connection factory for Cassandra
+spark.conf.set("spark.cassandra.connection.factory", "com.microsoft.azure.cosmosdb.cassandra.CosmosDbConnectionFactory")
+
+import org.apache.spark.sql.streaming.{OutputMode, Trigger}
+
+import java.util.Calendar
+
+//Write tuning parameters
+spark.conf.set("spark.cassandra.output.batch.size.rows", "1")//Leave this as one for Cosmos DB
+spark.conf.set("spark.cassandra.connection.connections_per_executor_max", "2")//Maximum number of connections per Host set on each Executor JVM - default parallelism/executors for Spark Commands
+spark.conf.set("spark.cassandra.output.concurrent.writes", "2")//Maximum number of batches executed in parallel by a single Spark task
+spark.conf.set("spark.cassandra.output.batch.grouping.buffer.size", "300")//How many batches per single Spark task can be stored in memory before sending to Cassandra
+spark.conf.set("spark.cassandra.connection.keep_alive_ms", "5000") //Period of time to keep unused connections open
+//cosmosdb_provisioned_throughput=500000
+
+/*
+//Option 1
+val query = consumableDF.writeStream
   .option("checkpointLocation", dbfsCheckpointDirPath)
-  .start(dbfsDestDirPath)
+ .format("org.apache.spark.sql.cassandra")
+  .options(Map( "table" -> "crimes_chicago_stream", "keyspace" -> "crimes_ks"))
+  .outputMode(OutputMode.Update)
+  .start()
+  
+  //DOES NOT WORK; This probably only works with DSE;
+  //java.lang.UnsupportedOperationException: Data source org.apache.spark.sql.cassandra does not support streamed writing
+*/
+
+
+val query = consumableDF.writeStream
+  .cassandraFormat("crimes_chicago_stream", "crimes_ks")
+  .option("checkpointLocation", dbfsCheckpointDirPath)
+  .outputMode(OutputMode.Update)
+  .start()
+  
 
 // COMMAND ----------
 
-// MAGIC %md
-// MAGIC ##### 3.0.2. Read from delta table
-// MAGIC Run the below in a different notebook while the stream process is running:
-
-// COMMAND ----------
-
-// MAGIC %sql
-// MAGIC CREATE DATABASE IF NOT EXISTS crimes_db;
-// MAGIC 
-// MAGIC USE crimes_db;
-// MAGIC 
-// MAGIC DROP TABLE IF EXISTS chicago_crimes_delta_stream;
-// MAGIC CREATE TABLE chicago_crimes_delta_stream
-// MAGIC USING DELTA
-// MAGIC LOCATION 'mnt/data/crimes/curatedDir/chicago-crimes-stream-delta';
-// MAGIC 
-// MAGIC OPTIMIZE chicago_crimes_delta_stream;
-// MAGIC 
-// MAGIC select * from crimes_db.chicago_crimes_delta_stream;
