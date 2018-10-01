6320a98750d96601fa32a6c5a1ed1b3c1dd33b91
Anagha Khanolkar <ankhanol@microsoft.com>
2018-10-01 14:58:14 -0500

    Update: README.md data-engineering data-science nosql

--- nosql/cosmosdb/04-Cassandra-API/scala/00-HowTo/2.10-StreamIngest-FromKafka-Part1.scala
+++ nosql/cosmosdb/04-Cassandra-API/scala/00-HowTo/2.10-StreamIngest-FromKafka-Part1.scala
@@ -3,12 +3,13 @@
 // MAGIC # What's in this exercise
 // MAGIC A pre-requisite for this module is to complete the first notebook for "Bulk load from blob" that covers downloading and curating the public dataset.<BR>
 // MAGIC   
-// MAGIC This is part 1 of 3 notebooks that demonstrate stream from Kafka, with structured streaming, of 6.7 million records/1.5 GB of the Chicago crimes public dataset.<BR>
-// MAGIC - In this notebook, will publish curated Chicago crimes data to Kafka<BR>
-// MAGIC - In notebook 2, we will use Spark structured streaming to ingest from Kafka and persist to Azure Cosmos DB Cassandra API<BR>
-// MAGIC - In notebook 3, we will use Spark structured streaming to ingest from Kafka and persist to a Databricks Delta table<BR>
+// MAGIC This is part 1 of 4 notebooks that demonstrate stream from Kafka, with structured streaming & classic spark streaming, of 6.7 million records/1.5 GB of the Chicago crimes public dataset.<BR>
+// MAGIC - In this notebook, we will publish curated Chicago crimes data to Kafka for purpose of the exercise<BR>
+// MAGIC - In notebook 2, we will (attempt to) ingest from Kafka using structured stream processing and persist to an Azure Cosmos DB Cassandra table<BR>
+// MAGIC - In notebook 3, we will ingest from Kafka using classic stream processing (DStream based) and persist to an Azure Cosmos DB Cassandra table<BR>
+// MAGIC - In notebook 4, we will ingest from Kafka using structured stream processing and persist to a Databricks Delta table<BR>
 // MAGIC   
-// MAGIC With the three notebooks, we cover (publishing to Kafka and then), reading from Kafka in streaming mode and sinking to an OLTP store - the Azure Cosmos DB Cassandra table, and an analytics store - the Databricks Delta table.<BR>
+// MAGIC With the four notebooks, we cover (publishing to Kafka and then), reading from Kafka in streaming mode and sinking to an OLTP store - the Azure Cosmos DB Cassandra table, and an analytics store - the Databricks Delta table.<BR>
 // MAGIC Part 1 covers provisioning and configuring HDInsight Kafka for the exercise, also Azure Cosmos DB Cassandra API.
 
 // COMMAND ----------
