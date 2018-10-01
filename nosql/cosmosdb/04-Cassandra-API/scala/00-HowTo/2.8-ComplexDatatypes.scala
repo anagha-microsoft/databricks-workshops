16204269eec1d73095b5f0acd508094bb1fc6d1b
Anagha Khanolkar <ankhanol@microsoft.com>
2018-10-01 15:58:10 -0500

    Update: README.md data-engineering data-science nosql

---
+++ nosql/cosmosdb/04-Cassandra-API/scala/00-HowTo/2.8-ComplexDatatypes.scala
@@ -0,0 +1,10 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC # What's in this exercise
+// MAGIC Basics of how to work with Azure Cosmos DB - Cassandra API from Databricks in batch.<br>
+// MAGIC 8.0. Complex datatypes<BR>
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC Work in progress
