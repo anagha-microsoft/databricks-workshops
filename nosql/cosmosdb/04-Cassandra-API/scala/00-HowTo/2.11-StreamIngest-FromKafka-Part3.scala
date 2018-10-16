8fbd219c92ea0c6467465d0bfab0561d2a7c1364
Anagha Khanolkar <ankhanol@microsoft.com>
2018-10-16 10:44:16 -0500

    Update: README.md data-engineering data-science iot nosql

--- nosql/cosmosdb/04-Cassandra-API/scala/00-HowTo/2.11-StreamIngest-FromKafka-Part3.scala
+++ nosql/cosmosdb/04-Cassandra-API/scala/00-HowTo/2.11-StreamIngest-FromKafka-Part3.scala
@@ -4,7 +4,7 @@
 // MAGIC This is part 2 of 4 notebooks that demonstrate stream ingest from Kafka, of 1.5 GB of the Chicago crimes public dataset.<BR>
 // MAGIC - In notebook 1, we published data to Kafka for purpose of the exercise<BR>
 // MAGIC - In notebook 2, we attempted to ingest from Kafka using structured stream processing and persist to an Azure Cosmos DB Cassandra table<BR>
-// MAGIC - In **this notebook**, we will ingest from Kafka using classic/legacy stream processing and persist to an Azure Cosmos DB Cassandra table<BR>
+// MAGIC - In **this notebook**, we will ingest from Kafka using classic/legacy stream processing (DStream based) and persist to an Azure Cosmos DB Cassandra table<BR>
 // MAGIC - In notebook 4, we will ingest from Kafka using structured stream processing and persist to a Databricks Delta table<BR>
 // MAGIC   
 // MAGIC While the Azure Cosmos DB Cassandra table serves as a hot store for OLTP, the Delta table will serve as an analytics store.<BR><BR>
@@ -12,13 +12,56 @@
 
 // COMMAND ----------
 
-//Cassandra connector
+package com.microsoft.cassandraapi.parsers 
+
+import org.apache.spark.sql.Row
+object CrimeParser {
+  case class ChicagoCrime(
+    case_id: Int, 
+    case_nbr: String, 
+    case_dt_tm: String,
+    block: String,
+    iucr: String,
+    primary_type: String,
+    description: String,
+    location_description: String,
+    arrest_made: Boolean,
+    was_domestic: Boolean,
+    beat: Int,
+    district: Int,
+    ward: Int,
+    community_area: Int,
+    fbi_code: String,
+    x_coordinate: Int,
+    y_coordinate: Int,
+    year: Int,
+    updated_dt: String,
+    latitude: Double,
+    longitude: Double,
+    location_coordinates: String,
+    case_timestamp: java.sql.Timestamp,
+    case_month: Int,
+    case_day_of_month: Int,
+    case_hour: Int,
+    case_day_of_week_nbr: Int,
+    case_day_of_week_name: String)
+
+  object ChicagoCrime {
+    def apply(r: Row): ChicagoCrime = ChicagoCrime(
+      r.getInt(0), r.getString(1), r.getString(2), r.getString(3), r.getString(4), r.getString(5), r.getString(6), r.getString(7), r.getBoolean(8), r.getBoolean(9), r.getInt(10), r.getInt(11), r.getInt(12), r.getInt(13), r.getString(14), r.getInt(15), r.getInt(16), r.getInt(17), r.getString(18), r.getDouble(19), r.getDouble(20), r.getString(21), r.getTimestamp(22), r.getInt(23), 
+      r.getInt(24), r.getInt(25), r.getInt(26), r.getString(27))
+  }
+} 
+
+// COMMAND ----------
+
+//datastax Spark connector
 import com.datastax.spark.connector._
 import com.datastax.spark.connector.cql.CassandraConnector
 import org.apache.spark.sql.cassandra._
 import com.datastax.spark.connector.streaming._
 
-//CosmosDB library for multiple retry to Cassandra
+//CosmosDB library for multiple retry
 import com.microsoft.azure.cosmosdb.cassandra
 
 //Kafka related
@@ -33,14 +76,14 @@ import org.apache.spark.storage.StorageLevel
 import org.apache.spark._
 import org.apache.spark.storage._
 import org.apache.spark.streaming._
-import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.receiver._
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.Row
 import spark.implicits._
-import java.util.UUID
+
+//import com.microsoft.cassandraapi.parsers 
 
 //Specify connection factory for Cassandra
 spark.conf.set("spark.cassandra.connection.factory", "com.microsoft.azure.cosmosdb.cassandra.CosmosDbConnectionFactory")
@@ -54,55 +97,73 @@ spark.conf.set("spark.cassandra.connection.keep_alive_ms", "5000") //Period of t
 
 // COMMAND ----------
 
+// MAGIC %md
+// MAGIC ### 1.0. Read from Kafka with spark classic (legacy) streaming (DStream API)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ##### 1.0.1. Topic, broker list, checkpoint directory
+
+// COMMAND ----------
+
+//1) Topic and broker list
+val crimesKafkaTopicArray = Array("crimes_chicago")
+//Replace the below with specifics for your broker instances
+val crimesKafkaBrokerAndPortCSV = "10.7.0.12:9092,10.7.0.13:9092,10.7.0.14:9092,10.7.0.15:9092"
+
+//2) Destination directory for checkpoints
+val dbfsCheckpointDirPath="/mnt/data/crimes/checkpointDir/chicago-crimes-stream-cassandra-kafka/"
+dbutils.fs.rm(dbfsCheckpointDirPath, recurse=true)
+val dbfsDestDirPath="/mnt/data/crimes/curatedDir/chicago-crimes-stream-cassandra-kafka-legacy/"
+dbutils.fs.rm(dbfsDestDirPath, recurse=true)
+
+//3) Other streaming related conf
 val streamingBatchIntervalSeconds = 5
 val streamingMaxRateOfIngest = "1000" //Max rate of ingest from Kafka - needs to be a string
-val crimesKafkaTopicArray = Array("crimes_chicago")
-val crimesKafkaBrokerAndPortCSV = "10.7.0.12:9092,10.7.0.13:9092,10.7.0.14:9092,10.7.0.15:9092" //Replace with your broker instances
-val checkPointDirectory = "/mnt/data/crimes/checkpointDir/chicago-crimes-stream-kafka-consumer/"
-val startDateTime =  java.time.LocalDateTime.now //Current date and time
 
 // COMMAND ----------
 
-def createSparkStreamingContext(
-    checkpointDirectory: String,
-    batchIntervalSeconds: Int,
-    maxRateOfIngest: String,
-    applicationName: String,
-    sparkSession: SparkSession): StreamingContext = {
-
-    val ssc = new StreamingContext(sparkSession.sparkContext, Seconds(batchIntervalSeconds))
-    //ssc.checkpoint(checkpointDirectory)
-    ssc
-  }
+// MAGIC %md
+// MAGIC ##### 1.0.2. Create a Direct DStream
 
 // COMMAND ----------
 
-//val ssc = new StreamingContext(sc, Seconds(streamingBatchIntervalSeconds))
-dbutils.fs.rm(checkPointDirectory, recurse=true) //In case you need to
-val ssc = StreamingContext.getOrCreate(checkPointDirectory,() => createSparkStreamingContext(checkPointDirectory,streamingBatchIntervalSeconds,
-        streamingMaxRateOfIngest, "CrimesIngestionFromKafkaSample", spark))
+import java.util.UUID
 
 val kafkaParams = Map[String, Object](
   "bootstrap.servers" -> crimesKafkaBrokerAndPortCSV,
   "key.deserializer" -> classOf[StringDeserializer],
   "value.deserializer" -> classOf[StringDeserializer],
-  "auto.offset.reset" -> "latest",
+  "auto.offset.reset" -> "earliest",
   "group.id" -> UUID.randomUUID().toString,
-  "enable.auto.commit" -> (false: java.lang.Boolean))
+  "enable.auto.commit" -> (false: java.lang.Boolean)
+)
+
+val batchInterval = Seconds(streamingBatchIntervalSeconds)
+val ssc = new StreamingContext(sc, batchInterval)
 
 val crimesRawDstream = KafkaUtils.createDirectStream[String, String](
   ssc,
   PreferConsistent,
-  Subscribe[String, String](crimesKafkaTopicArray, kafkaParams))
-
-val crimesDstream = crimesRawDstream.map(record =>  (record.value)) //Only payload
+  Subscribe[String, String](crimesKafkaTopicArray, kafkaParams)
+)
 
 //Accumulator for crime count
 val totalCrimeCount = ssc.sparkContext.longAccumulator("Number of crimes received")
 //Counter for elapsed time in seconds
-//var secondCounter: Long = streamingBatchIntervalSeconds
+var secondCounter: Long = streamingBatchIntervalSeconds
+
 
 // COMMAND ----------
+
+// MAGIC %md
+// MAGIC ##### 1.0.3. Parse and persist
+
+// COMMAND ----------
+
+//Extract only the payload
+val crimesDstream = crimesRawDstream.map(record =>  (record.value))
 
 //Parse json and persist to Cosmos DB
 crimesDstream.foreachRDD { rdd =>
@@ -144,9 +205,10 @@ crimesDstream.foreachRDD { rdd =>
           
           consumableDF.write.mode("append").format("org.apache.spark.sql.cassandra").options(Map( "table" -> "crimes_chicago_stream_kafka", "keyspace" -> "crimes_ks")).save()
 
-      }
-      println(s"TOTAL CRIMES FROM " + startDateTime + " TO " + java.time.LocalDateTime.now + " is: " + totalCrimeCount)
+      }//if (!rdd.isEmpty()) 
+      println(s"TOTAL CRIMES TO DATE FROM THE PAST " + secondCounter + " SECONDS: " + totalCrimeCount)
       println("=================================================================================")
+      secondCounter = secondCounter + streamingBatchIntervalSeconds
 }
 crimesDstream.print
 ssc.start()
@@ -154,5 +216,4 @@ ssc.awaitTermination()
 
 // COMMAND ----------
 
-//Need this if I ever need to rerun after clearing state
 //ssc.stop()
