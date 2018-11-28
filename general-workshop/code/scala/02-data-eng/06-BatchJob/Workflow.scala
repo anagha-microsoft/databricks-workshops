fee4fa3fe55d1aa21899fefb6a57d1994b0bb57c
Anagha Khanolkar <ankhanol@microsoft.com>
2018-11-27 22:45:13 -0600

    Update: README.md data-engineering data-science general-workshop iot nosql

--- general-workshop/code/scala/02-data-eng/06-BatchJob/Workflow.scala
+++ general-workshop/code/scala/02-data-eng/06-BatchJob/Workflow.scala
@@ -7,20 +7,20 @@ val batchID: Long = generateBatchID()
 
 // COMMAND ----------
 
-sql("insert into taxi_reports_db.BATCH_JOB_HISTORY VALUES(" + batchID + ", 1, 'Execute report 1','Started', CURRENT_TIMESTAMP)")
+insertBatchMetadata(1,1,"Execute report 1","Started")
 val executionStatusReport1 = dbutils.notebook.run("Report-1", 60)
 
 if(executionStatusReport1 == "Pass")
-  sql("insert into taxi_reports_db.BATCH_JOB_HISTORY VALUES(" + batchID + ", 1, 'Execute report 1','Completed', CURRENT_TIMESTAMP)")
+  insertBatchMetadata(1,1,"Execute report 1","Completed")
 
 // COMMAND ----------
 
 var executionStatusReport2 = "-"
 if(executionStatusReport1 == "Pass")
 {
-  sql("insert into taxi_reports_db.BATCH_JOB_HISTORY VALUES(" + batchID + ", 2, 'Execute report 2','Started', CURRENT_TIMESTAMP)")
+  insertBatchMetadata(1,2,"Execute report 2","Started")
   executionStatusReport2 = dbutils.notebook.run("Report-2", 60)
-  sql("insert into taxi_reports_db.BATCH_JOB_HISTORY VALUES(" + batchID + ", 2, 'Execute report 2','Completed', CURRENT_TIMESTAMP)")
+  insertBatchMetadata(1,2,"Execute report 2","Completed")
   
 }
 
