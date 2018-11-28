b3883e4cc3db35482176a234267527150dc310db
Anagha Khanolkar <ankhanol@microsoft.com>
2018-11-27 20:46:49 -0600

    Update: README.md data-engineering data-science general-workshop iot nosql

--- general-workshop/code/scala/02-data-eng/01-General/1-CreateDatabaseObjects.scala
+++ general-workshop/code/scala/02-data-eng/01-General/1-CreateDatabaseObjects.scala
@@ -8,7 +8,7 @@
 // COMMAND ----------
 
 // MAGIC %md
-// MAGIC ### 1. Create the taxi_db database
+// MAGIC ### 1. Create the taxi_db database in Databricks
 
 // COMMAND ----------
 
@@ -32,22 +32,42 @@ spark.catalog.listDatabases.show(false)
 // COMMAND ----------
 
 // MAGIC %md
-// MAGIC ### 2. Create external table definition for a remote Azure SQL database table over JDBC
-// MAGIC 
-// MAGIC ###### TODO: secure credentials
+// MAGIC ### 2. Create tables in Azure SQL database table from the portal - data explorer
 
 // COMMAND ----------
 
-// MAGIC %sql
-// MAGIC CREATE DATABASE IF NOT EXISTS taxi_reports_db;
-// MAGIC USE taxi_reports_db;
-// MAGIC 
-// MAGIC DROP TABLE IF EXISTS BATCH_JOB_HISTORY;
-// MAGIC CREATE TABLE BATCH_JOB_HISTORY
-// MAGIC USING org.apache.spark.sql.jdbc
-// MAGIC OPTIONS (
-// MAGIC   url 'jdbc:sqlserver://bhoomidbsrvr.database.windows.net:1433;database=bhoomidb;',
-// MAGIC   dbtable 'BATCH_JOB_HISTORY',
-// MAGIC   user 'akhanolk',
-// MAGIC   password "A1rawat#123"
+// MAGIC %md
+// MAGIC Create the following 3 tables:<br>
+// MAGIC BATCH_JOB_HISTORY => Persist ETL job metadata<br>
+// MAGIC TRIPS_BY_YEAR     => Report<br>
+// MAGIC TRIPS_BY_HOUR     => Report<br>
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ```
+// MAGIC DROP TABLE IF EXISTS dbo.BATCH_JOB_HISTORY;
+// MAGIC CREATE TABLE BATCH_JOB_HISTORY(
+// MAGIC batch_id int,
+// MAGIC batch_step_id int,
+// MAGIC batch_step_description varchar(100),
+// MAGIC batch_step_status varchar(30),
+// MAGIC batch_step_time varchar(30)
 // MAGIC );
+// MAGIC 
+// MAGIC DROP TABLE IF EXISTS TRIPS_BY_YEAR;
+// MAGIC CREATE TABLE TRIPS_BY_YEAR(
+// MAGIC taxi_type varchar(30),
+// MAGIC trip_year int,
+// MAGIC trip_count bigint
+// MAGIC );
+// MAGIC 
+// MAGIC DROP TABLE IF EXISTS TRIPS_BY_HOUR;
+// MAGIC CREATE TABLE TRIPS_BY_HOUR(
+// MAGIC taxi_type varchar(30),
+// MAGIC trip_year int,
+// MAGIC pickup_hour int,
+// MAGIC trip_count bigint
+// MAGIC );
+// MAGIC 
+// MAGIC ```
