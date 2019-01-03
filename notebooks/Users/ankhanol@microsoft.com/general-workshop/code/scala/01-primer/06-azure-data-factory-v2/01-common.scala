e29a61ccadf514842e06a9d0c2270273c14b7303
Anagha Khanolkar <ankhanol@microsoft.com>
2019-01-03 09:46:07 -0600

    Update: README.md data-engineering data-science general-workshop iot nosql notebooks

---
+++ notebooks/Users/ankhanol@microsoft.com/general-workshop/code/scala/01-primer/06-azure-data-factory-v2/01-common.scala
@@ -0,0 +1,19 @@
+// Databricks notebook source
+//JDBC connectivity related
+val driverClass = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
+val jdbcUsername = dbutils.secrets.get(scope = "gws-sql-db", key = "username")
+val jdbcPassword = dbutils.secrets.get(scope = "gws-sql-db", key = "password")
+val jdbcHostname = "gws-server.database.windows.net"
+val jdbcPort = 1433
+val jdbcDatabase = "gws_sql_db"
+
+// Create the JDBC URL without passing in the user and password parameters.
+val jdbcUrl = s"jdbc:sqlserver://${jdbcHostname}:${jdbcPort};database=${jdbcDatabase}"
+
+// Create a Properties() object to hold the parameters.
+import java.util.Properties
+val connectionProperties = new Properties()
+
+connectionProperties.put("user", s"${jdbcUsername}")
+connectionProperties.put("password", s"${jdbcPassword}")
+connectionProperties.setProperty("Driver", driverClass)
