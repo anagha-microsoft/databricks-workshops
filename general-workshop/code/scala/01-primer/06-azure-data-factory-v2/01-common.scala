b618f84871e9895bed96602852f8cc7c288baa3d
Anagha Khanolkar <ankhanol@microsoft.com>
2019-01-03 09:48:47 -0600

    Update: README.md data-engineering data-science general-workshop iot nosql

--- general-workshop/code/scala/01-primer/06-azure-data-factory-v2/01-common.scala
+++ general-workshop/code/scala/01-primer/06-azure-data-factory-v2/01-common.scala
@@ -3,9 +3,11 @@
 val driverClass = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
 val jdbcUsername = dbutils.secrets.get(scope = "gws-sql-db", key = "username")
 val jdbcPassword = dbutils.secrets.get(scope = "gws-sql-db", key = "password")
-val jdbcHostname = "gws-server.database.windows.net"
 val jdbcPort = 1433
 val jdbcDatabase = "gws_sql_db"
+
+//Replace with your server name
+val jdbcHostname = "gws-server.database.windows.net"
 
 // Create the JDBC URL without passing in the user and password parameters.
 val jdbcUrl = s"jdbc:sqlserver://${jdbcHostname}:${jdbcPort};database=${jdbcDatabase}"
