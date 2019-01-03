c3e312c2aa8b771bd5a992ea17029546ebaf43e8
Anagha Khanolkar <ankhanol@microsoft.com>
2019-01-03 09:50:32 -0600

    Update: README.md data-engineering data-science general-workshop iot nosql

---
+++ general-workshop/code/scala/01-primer/06-azure-data-factory-v2/03-generate-report-2.scala
@@ -0,0 +1,10 @@
+// Databricks notebook source
+//Generate report 2
+val reportDF = spark.sql("SELECT case_year,primary_type as case_type, count(*) AS crime_count FROM crimes_db.chicago_crimes_curated GROUP BY case_year,primary_type")
+
+// COMMAND ----------
+
+import org.apache.spark.sql.SaveMode
+
+//Persist report dataset to destination RDBMS
+reportDF.coalesce(1).write.mode(SaveMode.Overwrite).jdbc(jdbcUrl, "CHICAGO_CRIMES_COUNT_BY_YEAR", connectionProperties)
