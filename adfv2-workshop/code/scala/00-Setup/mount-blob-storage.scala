47658c63a23c861953956543f98d15385b11e224
Anagha Khanolkar <ankhanol@microsoft.com>
2019-01-13 22:25:30 -0600

    Update: README.md adfv2-workshop data-engineering data-science general-workshop iot nosql

---
+++ adfv2-workshop/code/scala/00-Setup/mount-blob-storage.scala
@@ -0,0 +1,96 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC # Mount blob storage
+// MAGIC 
+// MAGIC Mounting blob storage containers in Azure Databricks allows you to access blob storage containers like they are directories.<BR>
+// MAGIC   
+// MAGIC ### What's in this exercise?
+// MAGIC You will mount storage account containers required in the workshop.
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 1. Define credentials
+// MAGIC To mount blob storage - we need storage credentials - storage account name and storage account key
+
+// COMMAND ----------
+
+//Replace with your storage account name
+val storageAccountName = "adfv2wssa"
+val storageAccountAccessKey = "ni5XJFOMq6PXP1eTAbWI94KamN0njoylW5IvD5cJe+j7Qg3Sl35Y2TrT8AVxEmLYSpJSeaoExNEcH8NE/RN0Og=="
+
+// COMMAND ----------
+
+// MAGIC %fs
+// MAGIC mkdirs /mnt/workshop
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 2. Mount blob storage
+
+// COMMAND ----------
+
+// Check if already mounted
+display(dbutils.fs.ls("/mnt/workshop"))
+/*
+// Unmount if already mounted - as needed
+dbutils.fs.unmount("/mnt/workshop/raw/")
+dbutils.fs.unmount("/mnt/workshop/curated/")
+*/
+
+// COMMAND ----------
+
+//This is a function to mount a storage container
+def mountStorageContainer(storageAccount: String, storageAccountKey: String, storageContainer: String, blobMountPoint: String)
+{
+   try {
+     
+     println(s"Mounting ${storageContainer} to ${blobMountPoint}:")
+    // Unmount the storage container if already mounted
+    dbutils.fs.unmount(blobMountPoint)
+
+  } catch { 
+    //If this errors, the container is not mounted
+    case e: Throwable => println(s"....Container is not mounted; Attempting mounting now..")
+
+  } finally {
+    // Mount the storage container
+    val mountStatus = dbutils.fs.mount(
+    source = "wasbs://" + storageContainer + "@" + storageAccount + ".blob.core.windows.net/",
+    mountPoint = blobMountPoint,
+    extraConfigs = Map("fs.azure.account.key." + storageAccount + ".blob.core.windows.net" -> storageAccountKey))
+  
+    println("...Status of mount is: " + mountStatus)
+  }
+}
+
+// COMMAND ----------
+
+//Mount the various storage containers created
+mountStorageContainer(storageAccountName,storageAccountAccessKey,"raw","/mnt/workshop/raw")
+mountStorageContainer(storageAccountName,storageAccountAccessKey,"curated","/mnt/workshop/curated")
+
+// COMMAND ----------
+
+//Display directories
+display(dbutils.fs.ls("/mnt/workshop"))
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 3. Refresh mount points
+
+// COMMAND ----------
+
+//Refresh mounts if applicable
+//dbutils.fs.refreshMounts()
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 4. How to unmount
+
+// COMMAND ----------
+
+//dbutils.fs.unmount(<yourMountPoint>)
