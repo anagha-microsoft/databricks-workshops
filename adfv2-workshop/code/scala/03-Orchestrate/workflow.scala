7b6e12e014915a4bcedb4bfa7743eeccbf6c206d
Anagha Khanolkar <ankhanol@microsoft.com>
2019-01-14 12:38:27 -0600

    Update: README.md adfv2-workshop data-engineering data-science general-workshop iot nosql

--- adfv2-workshop/code/scala/03-Orchestrate/workflow.scala
+++ adfv2-workshop/code/scala/03-Orchestrate/workflow.scala
@@ -34,7 +34,7 @@ var executionStatusNotebook3 = "-"
 //If load completes, execute the curation notebook
 if(executionStatusNotebook2 == "Pass")
 {
-  executionStatusReport3 = dbutils.notebook.run("../03-Reporting/run-report", 120)
+  executionStatusNotebook3 = dbutils.notebook.run("../03-Reporting/run-report", 120)
 }
 
 // COMMAND ----------
@@ -45,4 +45,4 @@ if(executionStatusNotebook2 == "Pass")
 // COMMAND ----------
 
 //Return the status of the execution of the notebook
-dbutils.notebook.exit(executionStatusReport3)
+dbutils.notebook.exit(executionStatusNotebook3)
