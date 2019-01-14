86489738bb870daf473a6d0d78e7f5e85a2545c1
Anagha Khanolkar <ankhanol@microsoft.com>
2019-01-13 23:11:34 -0600

    Update: README.md adfv2-workshop data-engineering data-science general-workshop iot nosql

---
+++ adfv2-workshop/code/scala/01-ELT/02-curate.scala
@@ -0,0 +1,5 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC ## Curate NYC taxi data
+// MAGIC In this notebook - we will do some basic transformations, augmenting of the raw NYC taxi dataset.<br>
+// MAGIC This is by no way complete, its merely for the purpose of demonstrating chaining a workflow.
