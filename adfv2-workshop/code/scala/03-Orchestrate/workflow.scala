19585e83f172027b417a06ec8f48070da91d4145
Anagha Khanolkar <ankhanol@microsoft.com>
2019-01-14 12:22:38 -0600

    Update: README.md adfv2-workshop data-engineering data-science general-workshop iot nosql

---
+++ adfv2-workshop/code/scala/03-Orchestrate/workflow.scala
@@ -0,0 +1,48 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC ### 1.  Extract from staging and load to raw information zone
+
+// COMMAND ----------
+
+//Execute the load
+val executionStatusNotebook1 = dbutils.notebook.run("../01-ELT/01-load", 120)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 2.  Transform the raw dataset
+
+// COMMAND ----------
+
+var executionStatusNotebook2 = "-"
+
+//If load completes, execute the curation notebook
+if(executionStatusNotebook1 == "Pass")
+{
+  executionStatusNotebook2 = dbutils.notebook.run("../01-ELT/02-curate", 120)
+}
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 3.  Run report and pipe to RDBMS
+
+// COMMAND ----------
+
+var executionStatusNotebook3 = "-"
+
+//If load completes, execute the curation notebook
+if(executionStatusNotebook2 == "Pass")
+{
+  executionStatusReport3 = dbutils.notebook.run("../03-Reporting/run-report", 120)
+}
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 4.  Exit with execution status
+
+// COMMAND ----------
+
+//Return the status of the execution of the notebook
+dbutils.notebook.exit(executionStatusReport3)
