fa778362006a8684726fb17f3a48af1ed2f798bf
Anagha Khanolkar <ankhanol@microsoft.com>
2019-01-13 23:18:08 -0600

    Update: README.md adfv2-workshop data-engineering data-science general-workshop iot nosql

--- adfv2-workshop/code/scala/02-Orchestrate/workflow.scala
+++ adfv2-workshop/code/scala/02-Orchestrate/workflow.scala
@@ -1,1 +1,18 @@
 // Databricks notebook source
+//Execute the load
+val executionStatusNotebook1 = dbutils.notebook.run("01-load", 60)
+
+// COMMAND ----------
+
+var executionStatusNotebook2 = "-"
+
+//If load completes, execute the curation notebook
+if(executionStatusNotebook1 == "Pass")
+{
+  executionStatusReport2 = dbutils.notebook.run("02-curate", 60)
+}
+
+// COMMAND ----------
+
+//Return the status of the execution of the notebook
+dbutils.notebook.exit(executionStatusNotebook2)
