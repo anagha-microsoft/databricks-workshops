f754c794df92884aa7f472e5cb24ea47bea806ab
Anagha Khanolkar <ankhanol@microsoft.com>
2019-01-19 17:59:56 -0600

    Update: README.md adfv2-workshop data-engineering data-science general-workshop iot nosql

--- general-workshop/code/scala/01-primer/06-azure-data-factory-v2/02-generate-report-2.scala
+++ general-workshop/code/scala/01-primer/06-azure-data-factory-v2/02-generate-report-2.scala
@@ -1,27 +1,9 @@
 // Databricks notebook source
-//dbutils.widgets.removeAll
-
-// COMMAND ----------
-
-//Create widget for batch id
-dbutils.widgets.text("new_batch_id","")
-
-// COMMAND ----------
-
-//Capture batch ID
-val batchID: Int = dbutils.widgets.get("new_batch_id").toInt
-
-// COMMAND ----------
-
 // MAGIC %run ./00-common
 
 // COMMAND ----------
 
-insertBatchMetadata(batchID,2,"Execute report 2","Started")
-
-// COMMAND ----------
-
-//Generate report 2
+//Generate report
 val reportDF = spark.sql("SELECT case_year,primary_type as case_type, count(*) AS crime_count FROM crimes_db.chicago_crimes_curated GROUP BY case_year,primary_type")
 
 // COMMAND ----------
@@ -33,6 +15,4 @@ reportDF.coalesce(1).write.mode(SaveMode.Overwrite).jdbc(jdbcUrl, "CHICAGO_CRIME
 
 // COMMAND ----------
 
-//Mark as completed
-//In your code - enhance to ensure data did get persisted
-insertBatchMetadata(batchID,2,"Execute report 2","Completed")
+dbutils.notebook.exit("Pass")
