419c20b979619c48cd397eaaf3f38464cda32695
Anagha Khanolkar <ankhanol@microsoft.com>
2019-01-19 17:58:27 -0600

    Update: README.md adfv2-workshop data-engineering data-science general-workshop iot nosql

--- general-workshop/code/scala/01-primer/06-azure-data-factory-v2/01-generate-report-1.scala
+++ general-workshop/code/scala/01-primer/06-azure-data-factory-v2/01-generate-report-1.scala
@@ -3,15 +3,6 @@
 
 // COMMAND ----------
 
-//Capture batch ID
-val batchID: Int = generateBatchID()
-
-// COMMAND ----------
-
-insertBatchMetadata(batchID,1,"Execute report 1","Started")
-
-// COMMAND ----------
-
 //Generate report 1
 val reportDF = spark.sql("SELECT primary_type as case_type, count(*) AS crime_count FROM crimes_db.chicago_crimes_curated GROUP BY primary_type")
 
@@ -21,12 +12,6 @@ import org.apache.spark.sql.SaveMode
 
 //Persist report dataset to destination RDBMS
 reportDF.coalesce(1).write.mode(SaveMode.Overwrite).jdbc(jdbcUrl, "CHICAGO_CRIMES_COUNT", connectionProperties)
-
-// COMMAND ----------
-
-//Mark as completed
-//In your code - enhance to ensure data did get persisted
-insertBatchMetadata(batchID,1,"Execute report 1","Completed")
 
 // COMMAND ----------
 
