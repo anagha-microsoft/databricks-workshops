d3fc15f6c5de90ec3f2f7f657434ec0236816cec
Anagha Khanolkar <ankhanol@microsoft.com>
2019-01-25 23:30:34 -0600

    Update: README.md adfv2-workshop data-engineering data-science general-workshop iot msready2019 nosql

--- general-workshop/code/pyspark/01-primer/06-azure-data-factory-v2/00-common.py
+++ general-workshop/code/pyspark/01-primer/06-azure-data-factory-v2/00-common.py
@@ -1,21 +1,18 @@
 # Databricks notebook source
-//JDBC connectivity related
-val driverClass = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
-val jdbcUsername = dbutils.secrets.get(scope = "gws-sql-db", key = "username")
-val jdbcPassword = dbutils.secrets.get(scope = "gws-sql-db", key = "password")
-val jdbcPort = 1433
-val jdbcDatabase = "gws_sql_db"
+jdbcUsername = dbutils.secrets.get(scope = "gws-sql-db", key = "username")
+jdbcPassword = dbutils.secrets.get(scope = "gws-sql-db", key = "password")
+driverClass = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
 
-//Replace with your server name
-val jdbcHostname = "gws-server.database.windows.net"
+jdbcHostname = "gws-server.database.windows.net"
+jdbcPort = 1433
+jdbcDatabase = "gws_sql_db"
 
-// Create the JDBC URL without passing in the user and password parameters.
-val jdbcUrl = s"jdbc:sqlserver://${jdbcHostname}:${jdbcPort};database=${jdbcDatabase}"
+# Create the JDBC URL without passing in the user and password parameters.
+jdbcUrl = "jdbc:sqlserver://{0}:{1};database={2}".format(jdbcHostname, jdbcPort, jdbcDatabase)
 
-// Create a Properties() object to hold the parameters.
-import java.util.Properties
-val connectionProperties = new Properties()
-
-connectionProperties.put("user", s"${jdbcUsername}")
-connectionProperties.put("password", s"${jdbcPassword}")
-connectionProperties.setProperty("Driver", driverClass)
+#// Create a Properties() object to hold the parameters.
+connectionProperties = {
+  "user" : jdbcUsername,
+  "password" : jdbcPassword,
+  "driver" : driverClass
+}
