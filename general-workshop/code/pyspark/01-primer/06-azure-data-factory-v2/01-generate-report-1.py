0fa0224df8ab8783ccb7a6689f0cca2c2fdf3bb7
Anagha Khanolkar <ankhanol@microsoft.com>
2019-01-25 22:13:12 -0600

    Update: README.md adfv2-workshop data-engineering data-science general-workshop iot msready2019 nosql

--- general-workshop/code/pyspark/01-primer/06-azure-data-factory-v2/01-generate-report-1.py
+++ general-workshop/code/pyspark/01-primer/06-azure-data-factory-v2/01-generate-report-1.py
@@ -1,1 +1,16 @@
 # Databricks notebook source
+# MAGIC %run ./00-common
+
+# COMMAND ----------
+
+#Generate report
+reportDF = spark.sql("SELECT primary_type as case_type, count(*) AS crime_count FROM crimes_db.chicago_crimes_curated GROUP BY primary_type")
+
+# COMMAND ----------
+
+#Persist report dataset to destination RDBMS
+reportDF.coalesce(1).write.mode("overwrite").jdbc(jdbcUrl, "CHICAGO_CRIMES_COUNT", connectionProperties)
+
+# COMMAND ----------
+
+dbutils.notebook.exit("Pass")
