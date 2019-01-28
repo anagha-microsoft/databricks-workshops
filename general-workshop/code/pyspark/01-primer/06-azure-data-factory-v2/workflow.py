44e7f21901b7bf8bac4b06e028e4999c1f89a684
Thomas K Abraham <thomaskabraham@users.noreply.github.com>
2019-01-28 16:10:27 -0600

    Update: README.md adfv2-workshop data-engineering data-science general-workshop iot msready2019 nosql

--- general-workshop/code/pyspark/01-primer/06-azure-data-factory-v2/workflow.py
+++ general-workshop/code/pyspark/01-primer/06-azure-data-factory-v2/workflow.py
@@ -1,25 +1,11 @@
 # Databricks notebook source
-# MAGIC %run ./00-common
-
-# COMMAND ----------
-
-batchID = generateBatchID()
-
-# COMMAND ----------
-
-insertBatchMetadata(batchID,1,"Execute report 1","Started")
 executionStatusReport1 = dbutils.notebook.run("01-generate-report-1", 600)
-
-if executionStatusReport1 == "Pass":
-  insertBatchMetadata(batchID,1,"Execute report 1","Completed")
 
 # COMMAND ----------
 
 executionStatusReport2 = "-"
 if executionStatusReport1 == "Pass":
-  insertBatchMetadata(batchID,2,"Execute report 2","Started")
   executionStatusReport2 = dbutils.notebook.run("02-generate-report-2", 60)
-  insertBatchMetadata(batchID,2,"Execute report 2","Completed")
 
 # COMMAND ----------
 
