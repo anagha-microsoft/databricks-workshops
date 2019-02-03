ed0c3787a78f8175e7a30dc2f1b76f662966432e
Anagha Khanolkar <ankhanol@microsoft.com>
2019-02-02 19:14:55 -0600

    Update: README.md adfv2-workshop data-engineering data-science general-workshop iot msready2019 nosql

---
+++ msready2019/code/scala/streaming-delta-lab/02b-setup-azure-event-hub.scala
@@ -0,0 +1,93 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC ## 1.  Azure Event Hub
+// MAGIC **Pre-requisite:**<br>
+// MAGIC An Azure Event Hub should be available in your pre-provisioned lab environment.<br>
+// MAGIC If it does not exist, we will need to create the same.
+
+// COMMAND ----------
+
+// MAGIC %md The following are additional setup tasks for the workshop
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 1.1.  Create an Azure Event Hub instance in your Azure Event Hub namespace
+// MAGIC Name = aeh-nyc | Partitions = 3 | Retention period = 1
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://github.com/anagha-microsoft/databricks-workshops/raw/master/msready2019/images/1-create-aeh-1.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://github.com/anagha-microsoft/databricks-workshops/raw/master/msready2019/images/1-create-aeh-2.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://github.com/anagha-microsoft/databricks-workshops/raw/master/msready2019/images/1-create-aeh-3.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 1.2. Create a consumer group in your Azure Event Hub instance
+// MAGIC Name = nyc-aeh-topic-streaming-cg
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://github.com/anagha-microsoft/databricks-workshops/raw/master/msready2019/images/1-create-aeh-4.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://github.com/anagha-microsoft/databricks-workshops/raw/master/msready2019/images/1-create-aeh-5.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://github.com/anagha-microsoft/databricks-workshops/raw/master/msready2019/images/1-create-aeh-6.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://github.com/anagha-microsoft/databricks-workshops/raw/master/msready2019/images/1-create-aeh-7.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 1.3.  Create a Shared Access Policy for connectivity from Spark
+// MAGIC Policy Name = RootAccessPolicy | Manage, Listen, Send
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://github.com/anagha-microsoft/databricks-workshops/raw/master/msready2019/images/1-create-aeh-8.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://github.com/anagha-microsoft/databricks-workshops/raw/master/msready2019/images/1-create-aeh-9.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://github.com/anagha-microsoft/databricks-workshops/raw/master/msready2019/images/1-create-aeh-10.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://github.com/anagha-microsoft/databricks-workshops/raw/master/msready2019/images/1-create-aeh-11.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 1.4.  Capture the connection string with primary key for connectivity from Spark
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://github.com/anagha-microsoft/databricks-workshops/raw/master/msready2019/images/1-create-aeh-12.png)
