3c505428f701513ac7596a28fe3e1a98c629f3a2
Anagha Khanolkar <ankhanol@microsoft.com>
2019-02-02 19:55:31 -0600

    Initial commit

--- msready2019/code/scala/streaming-delta-lab/02b-setup-azure-event-hub.scala
+++ msready2019/code/scala/streaming-delta-lab/02b-setup-azure-event-hub.scala
@@ -10,6 +10,7 @@
 // MAGIC 2.  Within the Azure Event Hub instance, we will create a consumer group
 // MAGIC 3.  We will create a shared access policy for the Azure Event Hub instance
 // MAGIC 4.  We will capture the connection string for the primary key for use in Spark
+// MAGIC 5.  We will attach the Spark event hub connector to the cluster
 
 // COMMAND ----------
 
@@ -93,3 +94,40 @@
 
 // MAGIC %md
 // MAGIC ![](https://github.com/anagha-microsoft/databricks-workshops/raw/master/msready2019/images/1-create-aeh-12.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### 1.5.  Attach the Event Hub Spark connector library to the cluster
+
+// COMMAND ----------
+
+// MAGIC %md From within your Databricks workspace, create a library as detailed below-
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://github.com/anagha-microsoft/databricks-workshops/raw/master/msready2019/images/1-aeh-library-attach-1.png)
+
+// COMMAND ----------
+
+// MAGIC %md  Enter the maven coordinates of your Databricks Runtime compatible Azure Event Hub library.  Find the latest [here](https://docs.databricks.com/spark/latest/structured-streaming/streaming-event-hubs.html)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://github.com/anagha-microsoft/databricks-workshops/raw/master/msready2019/images/1-aeh-library-attach-2.png)
+
+// COMMAND ----------
+
+// MAGIC %md Select the cluster and click on install
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://github.com/anagha-microsoft/databricks-workshops/raw/master/msready2019/images/1-aeh-library-attach-3.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://github.com/anagha-microsoft/databricks-workshops/raw/master/msready2019/images/1-aeh-library-attach-4.png)
